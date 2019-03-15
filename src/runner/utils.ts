9f71951f20e8670a294409b0be06bd9242fd6492
Banou26 <thomas26_ds@msn.com>
2019-03-15 02:13:50 +0100

    Commit after old code cleaning

--- src/runner/utils.ts
+++
@@ -1,8 +0,0 @@
-import Path from 'path'
-
-export const prettifyPath = path => Path.relative(process.cwd(), path)
-
-export const prettifyTime = time =>
-  time < 1000
-    ? `${time.toFixed()}ms`
-    : `${(time / 1000).toFixed(2)}s`
