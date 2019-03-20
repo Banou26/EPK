4a6ca5c203de0d84e993a1691bf50db26a35e380
banou <thomas26_ds@msn.com>
2019-03-20 17:03:20 +0100

    Fix CLI action bug

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,11 +1,11 @@
 import epk from '../core/index'
-import { options, args } from './parser'
+import { action, options, args } from './parser'
 import cliReporter from './reporter'
 import logger from './logger'
 
 // @ts-ignore
 cliReporter({})(epk({
-  watch: options.action === 'serve',
+  watch: action === 'serve',
   target: options.target,
   entryFiles: args,
   browsers: options.browsers
