d1f1a8fbb38581e5901219ecacc222dfe2bef1e5
banou <thomas26_ds@msn.com>
2019-03-29 04:35:34 +0100

    fix: Display throws generated by parcel

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,12 +1,17 @@
 import epk from '../core/index'
 import { action, options, args } from './parser'
-import cliReporter from './reporter'
+import CliReporter from './reporter'
 import logger from './logger'
 
 // @ts-ignore
-cliReporter({})(epk({
-  watch: action === 'serve',
-  target: options.target,
-  entryFiles: args,
-  browsers: options.browsers
-})).subscribe(() => {}, err => logger.error(err))
+const cliReporter =
+  epk({
+    watch: action === 'serve',
+    target: options.target,
+    entryFiles: args,
+    browsers: options.browsers
+  })
+  // @ts-ignore
+  |> CliReporter({})
+
+cliReporter.subscribe(() => {}, err => logger.error(err))
