d0a9c6b61d1ba116b317f46ac68d23923c5d115d
banou <thomas26_ds@msn.com>
2019-06-16 22:19:10 +0200

    feat: Improve render of file tests status

--- src/reporters/cli/file.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Text, Box, Color } from 'ink'
-
-import { prettifyPath } from '../../utils/file.ts'
-
-export default ({ testFile }) =>
-  <Box>
-    {prettifyPath(testFile.name)}
-  </Box>
