689c69eb574d3556d9d5e31acc8e0f3742087a69
Banou <me@banou.dev>
2022-02-23 03:17:31 -0500

    fix: 🐛log override implementation causing infinite loop

--- src/runtime/console.ts
+++ src/runtime/console.ts
@@ -1,7 +1,9 @@
+const originalConsole = globalThis.console
+
 export const console = {
-  ...globalThis.console,
-  error: (...args) => globalThis.console.error('[EPK-LOG]', ...args),
-  warning: (...args) => globalThis.console.warn('[EPK-LOG]', ...args),
-  info: (...args) => globalThis.console.info('[EPK-LOG]', ...args),
-  log: (...args) => globalThis.console.log('[EPK-LOG]', ...args)
+  ...originalConsole,
+  error: (...args) => originalConsole.error('[EPK-LOG]', ...args),
+  warning: (...args) => originalConsole.warn('[EPK-LOG]', ...args),
+  info: (...args) => originalConsole.info('[EPK-LOG]', ...args),
+  log: (...args) => originalConsole.log('[EPK-LOG]', ...args)
 }
