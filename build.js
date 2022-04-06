b61cdf23af5ff892b1460ce41e1992549e70d3a4
Banou <me@banou.dev>
2022-04-06 00:21:01 -0400

    fix: 🐛try to fix issue with wrong import paths

--- build.js
+++ build.js
@@ -1,17 +1,55 @@
+import { relative } from 'path'
+
 import esbuild from 'esbuild'
+
+async function esbuildResolve(id, dir) {
+  let result;
+
+  await esbuild.build({
+    stdin: {
+      contents: `import ${JSON.stringify(id)}`,
+      resolveDir: dir
+    },
+    write: false,
+    bundle: true,
+    treeShaking: false,
+    ignoreAnnotations: true,
+    platform: 'node',
+    plugins: [
+      {
+        name: 'resolve',
+        setup({ onLoad }) {
+          onLoad({ filter: /.*/ }, (args) => {
+            result = args.path;
+            return { contents: '' };
+          });
+        }
+      }
+    ]
+  });
+  return result;
+}
 
 esbuild.build({
   watch: process.argv.includes('-w') || process.argv.includes('--watch'),
   bundle: true,
   format: 'esm',
   platform: 'node',
-  plugins: [{
-    name: 'make-all-packages-external',
-    setup(build) {
-      let filter = /^[^.\/]|^\.[^.\/]|^\.\.[^\/]/ // Must not start with "/" or "./" or "../"
-      build.onResolve({ filter }, args => ({ path: args.path, external: true }))
-    },
-  }],
+  // external: ['./node_modules/*'],
+  plugins: [
+    {
+      name: 'make-all-packages-external',
+      setup(build) {
+        let filter = /^[^.\/]|^\.[^.\/]|^\.\.[^\/]/ // Must not start with "/" or "./" or "../"
+        build.onResolve({ filter }, async args => {
+          if (args.path === 'playwright') return ({ path: 'playwright', external: true })
+          const resolvedPath = await esbuildResolve(args.path, '.')
+          const result = resolvedPath ? relative('./node_modules', resolvedPath).replaceAll('\\', '/') : args.path
+          return ({ path: result, external: true })
+        })
+      },
+    }
+  ],
   entryPoints: ['./src/cli/index.ts'],
   outfile: './build/cli.js',
   publicPath: '/',
