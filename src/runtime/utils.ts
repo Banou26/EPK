fb4b1b958646538ec8a3cd5500c10c179d02b1cd
Banou <me@banou.dev>
2022-05-09 17:54:55 -0400

    fix: 🐛toGlobal path

--- src/runtime/utils.ts
+++ src/runtime/utils.ts
@@ -1,4 +1,4 @@
 import type { Page } from 'playwright'
-import { toGlobal } from 'src/utils/runtime'
+import { toGlobal } from '../utils/runtime'
 
 export const waitForSelector: Page['waitForSelector'] = (...args) => globalThis[toGlobal('waitForSelector')](...args)
